4894fe271b64c08a218d63fab1d3a410f6ff5bda
Brian Onang'o <surgbc@gmail.com>
2021-04-27 23:06:23 +0300

    continue building site

--- src/extensions/buildSite.js
+++ src/extensions/buildSite.js
@@ -5,6 +5,36 @@ const fs = require('fs')
 // const escapeStringRegexp = require('escape-string-regexp')
 
 
+let specials = [
+    // order matters for these
+    "-"
+    , "["
+    , "]"
+    // order doesn't matter for any of these
+    , "/"
+    , "{"
+    , "}"
+    , "("
+    , ")"
+    , "*"
+    , "+"
+    , "?"
+    , "."
+    , "\\"
+    , "^"
+    , "$"
+    , "|"
+]
+
+    // I choose to escape every character with '\'
+    // even though only some strictly require it when inside of []
+    , regex = RegExp('[' + specials.join('\\') + ']', 'g')
+    ;
+
+const escapeRegExp = function (str) {
+    return str.replace(regex, "\\$&");
+};
+
 module.exports = toolbox => {
     let description = `csycms site --build  -n "site name"`
     toolbox.buildSite = async (print = true) => {
@@ -20,7 +50,7 @@ module.exports = toolbox => {
             return false
         }
         let siteConfig = await toolbox.readSiteConfig(false, siteName);
-        let { PORT, domain, scheme, staticSiteRepo , staticSiteLocation} = siteConfig;
+        let { PORT, domain, scheme, staticSiteRepo, staticSiteLocation } = siteConfig;
         if (!scheme.match(/http[s]*/)) scheme = http
         let siteUrl = `${scheme}://`
         if (domain.match(/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/) || domain === "localhost") {
@@ -57,56 +87,73 @@ module.exports = toolbox => {
         let promises = stdout.map(minifyAndSave);
         await to(Promise.all(promises))
 
+
+
         let staticSiteDomain = staticSiteLocation.split("://").slice(-1)[0]
         shell.exec(`echo ${staticSiteDomain} > /var/www/html/csycms/csycmsBuilds/${siteName}/CNAME`)
+        shell.exec(`echo ${staticSiteDomain} > /var/www/html/csycms/csycmsBuilds/${siteName}/docs/CNAME`)
         shell.exec(`cd /var/www/html/csycms/csycmsBuilds/${siteName}/docs && wget ${siteUrl}/robots.txt`)
+
+        output = shell.exec(`find /var/www/html/csycms/csycmsBuilds/${siteName}/docs -type f -name "*" |grep "\.html$"`)
+        stdout = output.stdout.split('\n');
+        stdout.map(file => {
+            if (file.match(/robots\.txt\.html$/)) shell.exec(`rm ${file}`)
+        })
+        output = shell.exec(`find /var/www/html/csycms/csycmsBuilds/${siteName}/docs -type f -name "*" |grep "\.html$"`)
+        stdout = output.stdout.split('\n');
+        // console.log(stdout)
+        stdout = stdout.filter(item => item !== '')
+        // console.log(stdout)
+
+        stdout.map(absolutePath => {
+            if(absolutePath.match(/index\.html/))return
+            let fileName = absolutePath.split("/").slice(-1)[0]           
+            let dirNameToUse = fileName.replace(/\.html$/, '')
+            let escapedString = `/${dirNameToUse}\.html/`;
+           
+            let dirFullPath = absolutePath.replace(/\/[^\/]*$/, `/${dirNameToUse}`)
+            // console.log({fileName, dirNameToUse, dirFullPath})
+            shell.exec(`mkdir -p "${dirFullPath}" && mv "${absolutePath}" "${dirFullPath}/index.html"`)
+            escapedString =  escapeRegExp(fileName);
+            // console.log({escapedString, escapedString1:escapedString.toString(), repls: escapeRegExp(siteUrl), siteUrl, dirNameToUse})
+            /** fix hrefs */
+            shell.exec(`find "${dirFullPath}/index.html" -type f -exec sed -i '/href="http/b; s/href="\\(.*\\)/href="..\\/\\1/g' {} +`)
+            shell.exec(`find "${dirFullPath}/index.html" -type f -exec sed -i '/src="http/b; s/src="\\(.*\\)/src="..\\/\\1/g' {} +`)
+            // shell.exec(`find /var/www/html/csycms/csycmsBuilds/${siteName}/docs -type f -exec sed -i 's/\\(src="\\)\\(^http\\)/\\1..\\2/g' {} +`)
+            /** remove .html from links */
+          
+            shell.exec(`find /var/www/html/csycms/csycmsBuilds/${siteName}/docs -type f -exec sed -i 's/${escapedString}/${dirNameToUse}/g' {} +`)
+            escapedString = escapedString.replace(/ /g, "&#32;")
+            //   console.log('--------->'+escapedString, dirNameToUse)
+            shell.exec(`find /var/www/html/csycms/csycmsBuilds/${siteName}/docs -type f -exec sed -i 's/${escapedString}/${dirNameToUse}/g' {} +`)
+            // console.log(`find /var/www/html/csycms/csycmsBuilds/${siteName} -type f -exec sed -i 's/${escapedString}/${dirNameToUse}/g' {} +`)
+
+            // console.log({dirNameToUse, dirFullPath, tmp: escapeRegExp(fileName), fileName, escapedString})
+        })
+
+        // output = shell.exec(`find /var/www/html/csycms/csycmsBuilds/${siteName}/docs -type f -name "*" |grep "\.html$"`)
+        // stdout = output.stdout.split('\n');
+        // console.log(stdout)
+        // stdout = stdout.filter(item => item !== '')
+        // console.log(stdout)
+
+        // return
         shell.exec(`cd /var/www/html/csycms/csycmsBuilds/${siteName}/docs && wget ${siteUrl}/sitemap.xml`)
 
         if (staticSiteRepo !== '') {
             let key = await toolbox.readSystemConfig()
             key = key.sshKey
             let message = `site build ${new Date().toISOString()}`
-            console.log(siteUrl.replace(/\//g, "\\/"))
-
-
-            let specials = [
-                // order matters for these
-                "-"
-                , "["
-                , "]"
-                // order doesn't matter for any of these
-                , "/"
-                , "{"
-                , "}"
-                , "("
-                , ")"
-                , "*"
-                , "+"
-                , "?"
-                , "."
-                , "\\"
-                , "^"
-                , "$"
-                , "|"
-            ]
-
-                // I choose to escape every character with '\'
-                // even though only some strictly require it when inside of []
-                , regex = RegExp('[' + specials.join('\\') + ']', 'g')
-                ;
-
-            const escapeRegExp = function (str) {
-                return str.replace(regex, "\\$&");
-            };
+            // console.log(siteUrl.replace(/\//g, "\\/"))
 
             const escapedString = escapeRegExp(siteUrl);
-            shell.exec(`find /var/www/html/csycms/csycmsBuilds/${siteName} -type f -exec sed -i 's/${escapedString}/${staticSiteLocation}/g' {} +`)
-            
+            shell.exec(`find /var/www/html/csycms/csycmsBuilds/${siteName}/docs -type f -exec sed -i 's/${escapedString}/${staticSiteLocation}/g' {} +`)
+
             shell.exec(`rm -rf /tmp/csycmsBuilds/${siteName}-swagger && mkdir -p  /tmp/csycmsBuilds/${siteName}-swagger && cd /tmp/csycmsBuilds/${siteName}-swagger && wget -mpEk ${siteUrl}/swagger-ui.html/ && cd /tmp/csycmsBuilds/${siteName}-swagger/${siteUrl.replace(/http[s]*:\/\//, '')} && mv swagger-ui.html  /var/www/html/csycms/csycmsBuilds/${siteName}/docs/`)
 
             shell.exec(`cd /var/www/html/csycms/csycmsBuilds/${siteName}  && git init `)
             shell.exec(`cd /var/www/html/csycms/csycmsBuilds/${siteName}  &&  git remote add origin ${staticSiteRepo} `)
-            shell.exec(`cd /var/www/html/csycms/csycmsBuilds/${siteName}  && git add . && git commit -m "${message}" && GIT_SSH_COMMAND='ssh -i ${key} -o IdentitiesOnly=yes' git push origin master -f`)
+            shell.exec(`cd /var/www/html/csycms/csycmsBuilds/${siteName}  && git add . && git commit -m "${message}" && GIT_SSH_COMMAND='ssh -i ${key} -o IdentitiesOnly=yes' git push origin master`)
         }
     }
 }
